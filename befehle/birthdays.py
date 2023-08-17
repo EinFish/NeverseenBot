28c4253fefe77a23c93b1d85c652e0f30712a990
EinFish <finley.alster2@gmail.com>
2023-08-17 13:17:30 +0200

    worked again on Birthdays.py

--- befehle/birthdays.py
+++ befehle/birthdays.py
@@ -24,25 +24,61 @@ class BirthdayCommands(discord.app_commands.Group):
         except FileNotFoundError:
             self.config = {'gratulation_channel': None}
 
-    @app_commands.command(name="setup", description="Erstelle ein Birthday System")
-    @commands.cooldown(1, 20, commands.BucketType.user)
-    async def start(self, ctx, channel: discord.TextChannel):
-        self.config['gratulation_channel'] = channel.id
-        save_data()
-        await ctx.send(f'Geburtstagssystem wurde gestartet. Gratulationen werden im Channel {channel.mention} gesendet.')
 
-    @app_commands.command(name="add", description="Füge deinen Geburtstag ins Geburtstagssystem ein")
-    @commands.cooldown(1, 20, commands.BucketType.user)
-    async def add(context, name, geburtstag):
-        birthdays[name] = geburtstag
-        save_data()
-        await context.send(f'Geburtstag von {name} wurde gespeichert.')
+    def save_data(self):
+        with open('birthdays.json', 'w') as file:
+            json.dump(self.birthdays, file, indent=4)
+        with open('config.json', 'w') as file:
+            json.dump(self.config, file, indent=4)
 
+
+@app_commands.command(name="setup", description="Erstelle ein Birthday System")
+@commands.cooldown(1, 20, commands.BucketType.user)
+async def bsetup(self, ctx, channel: discord.TextChannel):
+    self.config['gratulation_channel'] = channel.id
+    self.save_data()
+    await ctx.send(f'Geburtstagssystem wurde gestartet. Gratulationen werden im Channel {channel.mention} gesendet.')
+
+@app_commands.command(name="hinzu", description="Füge deinen Geburtstag ins Geburtstagssystem ein")
+@commands.cooldown(1, 20, commands.BucketType.user)
+async def badd(self, name, geburtstag):
+    self.birthdays[name] = geburtstag
+    self.save_data()
+    await self.bot.interaction.send(f'Geburtstag von {name} wurde gespeichert.')
+
+@app_commands.command(name="show", description="Zeigt den Geburtstag von einem Member")
+@commands.cooldown(1, 20, commands.BucketType.user)
+async def bshow(self, ctx, name, member: discord.Member = discord.Interaction.user.name):
+    if member in self.birthdays:
+        await ctx.send(f'{name} hat am {self.birthdays[name]} Geburtstag.')
+    else:
+        await ctx.send(f'Kein Geburtstag für {name} gefunden.')
+
+@app_commands.command(name="delete", description="Löscht den Geburtstag von einem Member")
+@commands.cooldown(1, 20, commands.BucketType.user)
+async def bdelete(self, ctx, name,  member: discord.Member = discord.Interaction.user.name):
+    if name in self.birthdays:
+        del self.birthdays[name]
+        self.save_data()
+        await ctx.send(f'Geburtstag von {name} wurde gelöscht.')
+    else:
+        await ctx.send(f'Kein Geburtstag für {name} gefunden.')
+
+@app_commands.command(name="next", description="Zeigt die nächsten Geburtstage an")
+@commands.cooldown(1, 20, commands.BucketType.user)
+async def bnext(self, ctx):
+    if len(self.birthdays) == 0:
+        await ctx.send('Keine Geburtstage gespeichert.')
+    else:
+        result = 'Gespeicherte Geburtstage:\n'
+        for name, geburtstag in self.birthdays.items():
+            result += f'{name}: {geburtstag}\n'
+        await ctx.send(result)
+
+ 
 
     
-
-
-class Birthday(commands.Cog):
+class BirthdayCog(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
@@ -56,7 +92,7 @@ class Birthday(commands.Cog):
 
 
 async def setup(bot):
-    await bot.add_cog(Birthday(bot))
+    await bot.add_cog(BirthdayCog(bot))
 
 
 
