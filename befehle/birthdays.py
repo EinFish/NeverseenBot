e1435bc0aa85e8ffa53c3b51aa86d5d09207f5fa
EinFish <finley.alster2@gmail.com>
2023-08-22 20:27:56 +0200

    worked on birthday.py and added serverconfig.json

--- befehle/birthdays.py
+++ befehle/birthdays.py
@@ -2,6 +2,7 @@ import asyncio
 import json
 from typing import Any, Dict, List, Optional, Union
 import discord
+import datetime
 from discord.app_commands.commands import Group
 from discord.app_commands.translator import locale_str
 from discord.ext import commands
@@ -11,33 +12,80 @@ from discord.utils import MISSING
 import utils
 
 birthdayjson = utils.birthdayinit()
+
+
 class BirthdayCommands(discord.app_commands.Group):
-    
+
     @app_commands.command(name="setup", description="Erstelle ein Birthday System")
     @commands.cooldown(1, 20, commands.BucketType.user)
-    async def bsetup(self, channel: discord.TextChannel):
-        print("dsf")
+    async def bsetup(self, interaction, birthdaychannel: discord.TextChannel):
+        await interaction.response.defer()
+        guildid = interaction.guild.id
+        guildname = interaction.guild.name
+        channelid = birthdaychannel.id
+        with open("serverconfig.json") as file:
+            bjson = json.load(file)
+            bjson[str(guildid)] = {"name": guildname, "bday": channelid}
+
+            print(bjson)
+
+            with open("serverconfig.json", 'w') as json_file:
+                json.dump(bjson, json_file, indent=4)
+
+            await interaction.followup.send(f"Birthday Channel gesetzt zu {birthdaychannel}")
+
     @app_commands.command(name="add", description="Füge deinen Geburtstag ins Geburtstagssystem ein")
     @commands.cooldown(1, 20, commands.BucketType.user)
     async def badd(self, interaction, tag: int, monat: int, jahr: int = None):
-        geburtstag = ""
-        birthdayjson.bdjson.birthdays[interaction.user.name] = geburtstag
-        birthdayjson.save_data()
+        userid = interaction.user.id
+        with open("birthdays.json") as file:
+            bjson = json.load(file)
+            date_format = "%d/%m/%Y"
+            date_format2 = "%d/%m"
+            if not jahr == None:
+                today = datetime.datetime.now()
+                bday = datetime.datetime.strptime(
+                    f"{monat}/{tag}/{jahr}", date_format)
+                diff = bday - today
+                diff.days
+                print(bday)
+                ts = datetime.datetime.timestamp(bday)
+                print(type(bday))
+                print(diff)
+                bjson[str(userid)] = {"bday": ts}
+                print(bjson)
+            else:
+                today = datetime.datetime.now()
+                bday = datetime.datetime.strptime(
+                    f"{monat}/{tag}", date_format2)
+                diff = bday - today
+                diff.days
+                print(bday)
+                ts = datetime.datetime.timestamp(bday)
+                print(type(bday))
+                print(diff)
+                bjson[str(userid)] = {"bday": ts}
+                print(bjson)
 
+                with open("serverconfig.json", 'w') as json_file:
+                    json.dump(bjson, json_file, indent=4)
 
     @app_commands.command(name="show", description="Zeigt den Geburtstag von einem Member")
     @commands.cooldown(1, 20, commands.BucketType.user)
     async def bshow(self, member: discord.Member):
         print("dsf")
+
     @app_commands.command(name="delete", description="Löscht den Geburtstag von einem Member")
     @commands.cooldown(1, 20, commands.BucketType.user)
     async def bdelete(self, member: discord.Member):
         print("dsf")
+
     @app_commands.command(name="next", description="Zeigt die nächsten Geburtstage an")
     @commands.cooldown(1, 20, commands.BucketType.user)
     async def bnext(self, interaction):
         print("dsf")
-        
+
+
 class BirthdayCog(commands.Cog):
 
     def __init__(self, bot):
@@ -45,8 +93,9 @@ class BirthdayCog(commands.Cog):
 
     @commands.Cog.listener()
     async def on_ready(self):
-        birthdaycmds = BirthdayCommands(name="birthday", description="Befehle des Birthday Systemes")
-        self.bot.tree.add_command(birthdaycmds)        
+        birthdaycmds = BirthdayCommands(
+            name="birthday", description="Befehle des Birthday Systemes")
+        self.bot.tree.add_command(birthdaycmds)
         print("Birthdays Geladen!")
 
 
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import discord
 import os
 import asyncio
-import yaml
+import json
 from discord.ext import commands
 
-bot = commands.Bot("neverseen.", intents=discord.Intents.all(), application_id="1135990698517221376")
+bot = commands.Bot("neverseen.", intents=discord.Intents.all(),
+                   application_id="1135990698517221376")
 
 
 if __name__ == "__main__":
@@ -12,17 +13,20 @@ if __name__ == "__main__":
     for extension in os.listdir(os.fsencode("befehle")):
         if os.fsdecode(extension).endswith(".py"):
             try:
-                asyncio.run(bot.load_extension(f"befehle.{os.fsdecode(extension)[:-3]}"))
+                asyncio.run(bot.load_extension(
+                    f"befehle.{os.fsdecode(extension)[:-3]}"))
             except:
-                print(f"Die extension {os.fsdecode(extension)[:-3]} konnte nicht geladen werden.")
+                print(
+                    f"Die extension {os.fsdecode(extension)[:-3]} konnte nicht geladen werden.")
                 raise
+
 
 @bot.event
 async def on_ready():
     print("Bot ist ready!")
     await bot.change_presence(activity=discord.Streaming(name="omg", url="https://www.twitch.tv/lefish9873"))
 
-with open("config.yml", "r") as file:
-    config = yaml.safe_load(file)
+with open("config.json", "r") as file:
+    config = json.load(file)
 
 bot.run(config["TOKEN"])
