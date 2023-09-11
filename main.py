1923ab17dddfaa408954a1b2ec610adc6a314b6a
EinFish <finley.alster2@gmail.com>
2023-09-11 19:41:57 +0200

    fixed birthdayloop error

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import discord
-import sys
+import datetime
+import time
 import os
 import asyncio
 import json
@@ -69,11 +70,79 @@ async def presences():
         await asyncio.sleep(30)
 
 
+@loop(hours=1)
+async def birthdayloop():
+    with open("serverconfig.json") as file:
+        sjson = json.load(file)
+
+    with open("users.json") as file:
+        bjson = json.load(file)
+
+    currenttime = datetime.datetime.fromtimestamp(
+        int(time.time())).strftime('%H')
+    if currenttime == "19":  # Es ist 3 Uhr morgends
+        try:
+            config = sjson  # config laden
+            for server in config:
+                try:
+
+                    roleid = int(config[server]["bdayrole"])
+                    channelid = int(config[server]["bday"])
+
+                    # <- Wenn es einen Ping geben soll
+                    pingid = int(config[server]["bdayrole"])
+                    guild = bot.get_guild(int(server))
+
+                    birthdayconfig = bjson  # Birthdayconfig von den Server laden
+
+                    birthdayMembers = []
+                    daytoday = datetime.datetime.now().strftime("%d")  # Heutiger Tag
+                    monthtoday = datetime.datetime.now().strftime("%m")  # Heutiger Monat
+                    for member in guild.members:
+                        try:
+                            bday = birthdayconfig[str(member.id)]["bday"]
+                            if str(daytoday) == bday.split("/")[0] and str(monthtoday) == bday.split("/")[1]:
+                                birthdayMembers.append(member)
+                        except:
+                            pass  # Mitglied hat den geburtstag nicht eingetragen
+                    if len(birthdayMembers) == 0:
+                        continue  # Es hat heute niemand auf den Server geburtstag
+
+                    try:
+                        role = guild.get_role(roleid)
+                        for member in birthdayMembers:
+                            try:
+                                await member.add_roles(role)
+                            except:
+                                pass  # Der Bot darf den Member nicht bearbeiten
+                    except:
+                        pass  # Die Rolle existiert nicht
+
+                    try:
+
+                        # <- Wenn ping gewünscht
+                        content = guild.get_role(int(pingid)).mention
+                        pass
+                    except:
+                        content = None
+
+                    channel = guild.get_channel(channelid)
+                    birthdayPings = ""
+                    for member in birthdayMembers:
+                        birthdayPings += f"{member.mention} "
+                    await channel.send(embed=discord.Embed(title="Herzlichen Glückwunsch!", description=f"{birthdayPings} {'Hat' if len(birthdayMembers) == 1 else 'Haben'} heute Geburtstag!!!", color=0x0094ff), content=content)
+                except:
+                    pass
+        except:
+            pass
+
+
 @bot.event
 async def on_ready():
     print("Bot is running!")
     check_twitch_online_streamers.start()
     presences.start()
+    birthdayloop.start()
 
 
 bot.run(config["TOKEN"])
